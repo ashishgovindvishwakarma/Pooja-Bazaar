d800c2ca7b7483d581a2875e07a5800d24094ba1
Ashish Vishwakarma <ashishgovindvishwakarma@gmail.com>
2026-03-03 09:58:17 +0530

    Add files via upload

---
+++ product .js
@@ -0,0 +1,73 @@
+// products.js
+const productsDB = [
+  {
+    name: "Havan ingredients or Incense",
+    price: "250",
+    image: "poojakit.png",
+    description: "Complete havan samagri kit including incense sticks, powders, and other essentials.",
+    link: "product.html"
+  },
+  {
+    name: "krishna idol1",
+    price: "600",
+    image: "krishnaidol1.png",
+    description: "Beautifully crafted idols and statues for pooja and decoration.",
+    link: "product.html"
+  },
+  {
+    name: "Pooja Clothes",
+    price: "400",
+    image: "kurtas.png",
+    description: "Traditional pooja clothes such as dhotis, kurtas, and sarees.",
+    link: "product.html"
+  },
+  {
+    name: "Thali",
+    price: "350",
+    image: "thali.png",
+    description: "Decorated pooja thali with diya, roli, chawal and other essentials.",
+    link: "product.html"
+  },
+  {
+    name: "Kalash",
+    price: "300",
+    image: "kalash.png",
+    description: "Sacred kalash for rituals, weddings, and pooja ceremonies.",
+    link: "product.html"
+  },
+  {
+    name: "Dhoop Stick",
+    price: "100",
+    image: "dhoopstick.png",
+    description: "Fragrant dhoop sticks for daily prayers and meditation.",
+    link: "product.html"
+  },
+  {
+    name: "laxmi idols",
+    price: "400",
+    image: "laxmi idol.png",
+    description: "Traditional pooja clothes such as dhotis, kurtas, and sarees.",
+    link: "product.html"
+  },
+  {
+    name: "shiva lord murti",
+    price: "400",
+    image: "shiva  idol.png",
+    description: "Traditional pooja clothes such as dhotis, kurtas, and sarees.",
+    link: "product.html"
+  },
+  {
+    name: "Mandir",
+    price: "1500",
+    image: "mandir.png",
+    description: "Wooden mandir for home with beautiful carvings.",
+    link: "product.html"
+  },
+  {
+    name: "Ganpati Idol",
+    price: "800",
+    image: "ganpati idol.png",
+    description: "Beautiful Ganpati idol for pooja and festivals.",
+    link: "product.html"
+  }
+];
